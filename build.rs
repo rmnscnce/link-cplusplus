8841011d84704264b94b46c55095af3e9134ca45
Paskal Sitepu <rmnscnce@ya.ru>
2023-12-28 18:56:42 +0700

    Statically link dummy.cc
    Signed-off-by: Paskal Sitepu <rmnscnce@ya.ru>

--- build.rs
+++ build.rs
@@ -28,7 +28,11 @@ fn main() {
             let out_dir = env::var_os("OUT_DIR").expect("missing OUT_DIR");
             let path = PathBuf::from(out_dir).join("dummy.cc");
             fs::write(&path, "int rust_link_cplusplus;\n").unwrap();
-            cc::Build::new().cpp(true).file(&path).compile("link-cplusplus");
+            cc::Build::new()
+                .cpp(true)
+                .file(&path)
+                .static_flag(true)
+                .compile("link-cplusplus");
         }
     }
 }
